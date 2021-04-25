4d024a19c3f5fc92d05449b203ae5040241b17a6
jennyjnxu <jennyjnxu@tencent.com>
2021-04-25 17:01:53 +0800

    go test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module test.com
+
+go 1.16
+
+require github.com/objcoding/testmod v1.0.1
---
+++ main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+"fmt"
+"github.com/objcoding/testmod"
+)
+
+func main() {
+	fmt.Println(testmod.Hi("张乘辉"))
+}
